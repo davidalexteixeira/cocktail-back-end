e086d9e893e4db5e79d7373a6d99ed444e52c645
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-03-01 22:08:37 +0100

    added images to seed file

--- bin/seeds.js
+++ bin/seeds.js
@@ -8,6 +8,7 @@ var drinks = [
   {
     name: 'Mojito',
     type: 'Rum',
+    imageUrl: 'http://www.tsugars.com/media/1188/nuestro-mojito-moreno-1024x815-1024x815.jpg',
     ingredients: [
       {
         name: 'Rum',
@@ -34,6 +35,7 @@ var drinks = [
   {
     name: 'Gin and Tonic',
     type: 'Gin',
+    imageUrl: 'http://blog.ticketea.com/wp-content/uploads/2016/06/gintonic.jpg',
     ingredients: [
       {
         name: 'Gin',
@@ -55,6 +57,7 @@ var drinks = [
   {
     name: 'Moscow Mule',
     type: 'Vodka',
+    imageUrl: 'https://i.pinimg.com/originals/e5/5e/c6/e55ec6477d3682a23c3ab216c8ced0b8.jpg',
     ingredients: [
       {
         name: 'Vodka',
@@ -76,6 +79,7 @@ var drinks = [
   {
     name: 'Mimosa',
     type: 'Champagne',
+    imageUrl: 'http://s3-us-west-2.amazonaws.com/s3.shermans.test/wlnkfm/styles/delta__775x515/s3/s3fs-public/dreamstime_s_72674914.jpg?itok=A6ccoUKz',
     ingredients: [
       {
         name: 'Champagne',
@@ -92,6 +96,7 @@ var drinks = [
   {
     name: 'Vodka Cran',
     type: 'Vodka',
+    imageUrl: 'https://www.inspiredtaste.net/wp-content/uploads/2011/11/Pear-and-Cranberry-Cocktail-Recipe-1200.jpg',
     ingredients: [
       {
         name: 'Vodka',
@@ -113,6 +118,7 @@ var drinks = [
   {
     name: 'Greyhound',
     type: 'Vodka',
+    imageUrl: 'https://f5petdcvm0-flywheel.netdna-ssl.com/wp-content/uploads/2012/12/IMG_1707.jpg',
     ingredients: [
       {
         name: 'Vodka',
@@ -129,6 +135,7 @@ var drinks = [
   {
     name: 'Cuba Libre',
     type: 'Rum',
+    imageUrl: 'https://drinks-dvq6ncf.netdna-ssl.com//wordpress/wp-content/uploads/2015/03/rum-and-coke.jpg',
     ingredients: [
       {
         name: 'Rum',
@@ -145,6 +152,7 @@ var drinks = [
   {
     name: 'Screwdriver',
     type: 'Vodka',
+    imageUrl: 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/9/5/0/CCADO102_Muddled-Screwdriver-Recipe_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371609418528.jpeg',
     ingredients: [
       {
         name: 'Vodka',
@@ -161,6 +169,7 @@ var drinks = [
   {
     name: 'Dark and Stormy',
     type: 'Rum',
+    imageUrl: 'https://cdn.vox-cdn.com/thumbor/JMHW04TJpdXNxmT2oS6zwJTJWsI=/898x600/cdn.vox-cdn.com/uploads/chorus_asset/file/2386450/20141022dark_n_stormy_-_schiller.0.jpg',
     ingredients: [
       {
         name: 'Rum',
@@ -177,6 +186,7 @@ var drinks = [
   {
     name: 'Gimlet',
     type: 'Gin',
+    imageUrl: 'http://cdn.liquor.com/wp-content/uploads/2017/04/18130027/gimlet-1200x628-social.jpg',
     ingredients: [
       {
         name: 'Gin',
