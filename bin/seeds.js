bab1540c33259aabbfb1ffdc100dbbc75209ea17
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-03-01 21:02:04 +0100

    added seeds.js

---
+++ bin/seeds.js
@@ -0,0 +1,189 @@
+import { ucs2 } from 'punycode';
+
+var drinks = [
+  {
+    name: 'Mojito',
+    type: 'Rum',
+    ingredients: [
+      {
+        name: 'Rum',
+        quantity: 1.5,
+        unit: 'ounces'
+      },
+      {
+        name: 'mint',
+        quantity: 2,
+        unit: 'leaves'
+      },
+      {
+        name: 'sprite',
+        quantity: 1,
+        unit: 'cup'
+      },
+      {
+        name: 'ice',
+        quantity: 0.5,
+        unit: 'cups'
+      }
+    ]
+  },
+  {
+    name: 'Gin and Tonic',
+    type: 'Gin',
+    ingredients: [
+      {
+        name: 'Gin',
+        quantity: 1.5,
+        unit: 'ounces'
+      },
+      {
+        name: 'tonic water',
+        quantity: 1,
+        unit: 'cup'
+      },
+      {
+        name: 'lemon',
+        quantity: 1,
+        unit: 'wedge'
+      }
+    ]
+  },
+  {
+    name: 'Moscow Mule',
+    type: 'Vodka',
+    ingredients: [
+      {
+        name: 'Vodka',
+        quantity: 1.5,
+        unit: 'ounces'
+      },
+      {
+        name: 'ginger ale',
+        quantity: 1,
+        unit: 'cup'
+      },
+      {
+        name: 'lime',
+        quantity: 2,
+        unit: 'wedges'
+      }
+    ]
+  },
+  {
+    name: 'Mimosa',
+    type: 'Champagne',
+    ingredients: [
+      {
+        name: 'Champagne',
+        quantity: 1,
+        unit: 'cup'
+      },
+      {
+        name: 'orange juice',
+        quantity: 1,
+        unit: 'cup'
+      }
+    ]
+  },
+  {
+    name: 'Vodka Cran',
+    type: 'Vodka',
+    ingredients: [
+      {
+        name: 'Vodka',
+        quantity: '1.5',
+        unit: 'ounces'
+      },
+      {
+        name: 'Cranberry juice',
+        quantity: 1,
+        unit: 'cup'
+      },
+      {
+        name: 'Lime',
+        quantity: 2,
+        unit: 'wedges'
+      }
+    ]
+  },
+  {
+    name: 'Greyhound',
+    type: 'Vodka',
+    ingredients: [
+      {
+        name: 'Vodka',
+        quantity: 1.5,
+        unit: 'ounces'
+      },
+      {
+        name: 'Grapefruit juice',
+        quantity: 1,
+        unit: 'cup'
+      }
+    ]
+  },
+  {
+    name: 'Cuba Libre',
+    type: 'Rum',
+    ingredients: [
+      {
+        name: 'Rum',
+        quantity: 1.5,
+        unit: 'ounces'
+      },
+      {
+        name: 'Cola',
+        quantity: 1,
+        unit: 'cup'
+      }
+    ]
+  },
+  {
+    name: 'Screwdriver',
+    type: 'Vodka',
+    ingredients: [
+      {
+        name: 'Vodka',
+        quantity: 1.5,
+        unit: 'ounces'
+      },
+      {
+        name: 'Orange juice',
+        quantity: 1,
+        unit: 'cup'
+      }
+    ]
+  },
+  {
+    name: 'Dark and Stormy',
+    type: 'Rum',
+    ingredients: [
+      {
+        name: 'Rum',
+        quantity: 1.5,
+        unit: 'ounces'
+      },
+      {
+        name: 'ginger ale',
+        quantity: 1,
+        unit: 'cup'
+      }
+    ]
+  },
+  {
+    name: 'Gimlet',
+    type: 'Gin',
+    ingredients: [
+      {
+        name: 'Gin',
+        quantity: 3,
+        unit: 'ounces'
+      },
+      {
+        name: 'lime juice',
+        quantity: 1,
+        unit: 'ounce'
+      }
+    ]
+  }
+];
